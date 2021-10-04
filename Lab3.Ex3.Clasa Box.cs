f3eff2209013259464cc7c6ba81ba5870b0a284d
Ponomarenco Elena <71901122+elenaponomarenco@users.noreply.github.com>
2021-10-04 10:26:38 +0300

    Add files via upload

---
+++ Lab3.Ex3.Clasa Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3.Ex_1
+{
+    class Box
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+
+        public Box()
+        {
+            Height = 1;
+            Width = 1;
+            Depth = 1;
+        }
+
+        public Box(double value)
+        {
+            Height = value;
+            Width = value;
+            Depth = value;
+        }
+
+        public Box(double height, double width, double depth)
+        {
+            Height = height;
+            Width = width;
+            Depth = depth;
+        }
+    }
+}
---
+++ Lab3.Ex3.Program Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab3.Ex_1
+{
+    class Program
+    {
+        public static double Surface_Area(Box x)
+        {
+            return 2 * (x.Depth + x.Height + x.Width);
+        }
+        public static double Volume(Box x)
+        {
+            return x.Width * x.Height * x.Depth;
+        }
+        static void Main(string[] args)
+        {
+            Box Obj1 = new Box();
+            Box Obj2 = new Box(2.5);
+            Box Obj3 = new Box(2.5, 3.75, 4.2);
+
+            Console.WriteLine("Box 1\n Width: {0}\n Height: {1}\n Depth: {2}", Obj1.Width, Obj1.Height, Obj1.Depth);
+            Console.WriteLine("Box 2\n Width: {0}\n Height: {1}\n Depth: {2}", Obj2.Width, Obj2.Height, Obj2.Depth);
+            Console.WriteLine("Box 3\n Width: {0}\n Height: {1}\n Depth: {2}", Obj3.Width, Obj3.Height, Obj3.Depth);
+            Console.WriteLine("\n1) Surface Area of the box 1:  " + Surface_Area(Obj1));
+            Console.WriteLine("2) Surface Area of the box 2:  " + Surface_Area(Obj2));
+            Console.WriteLine("3) Surface Area of the box 3:  " + Surface_Area(Obj3));
+            Console.WriteLine("\n1) Volume of the box 1:  " + Volume(Obj1));
+            Console.WriteLine("2) Volume of the box 2:  " + Volume(Obj2));
+            Console.WriteLine("3) Volume of the box 3:  " + Volume(Obj3));
+
+        }
+    }
+}
