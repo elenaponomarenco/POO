4f149a38d49cfb8e46e484002e7617547ed2d849
Ponomarenco Elena <71901122+elenaponomarenco@users.noreply.github.com>
2021-10-04 08:10:16 +0300

    Add files via upload

---
+++ Lab3.Ex1.Program Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab3.Ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box Obj1 = new Box();
+            Box Obj2 = new Box();
+            Box Obj3 = new Box();
+        }
+    }
+}
