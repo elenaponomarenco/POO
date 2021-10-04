69ab5faa80007f7acb150d0d91726a0ac3553f32
Ponomarenco Elena <71901122+elenaponomarenco@users.noreply.github.com>
2021-10-04 16:20:08 +0300

    Add files via upload

---
+++ Lab3.Ex3.Clasa Queue.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3.ex2
+{
+    class Queue
+    {
+        private static int Front { get; set; }
+        private static int Rear { get; set; }
+        private static int Capacity { get; set; }
+        private static int[] queue { get; set; }
+
+        public Queue(int size) //constructor cu parametru
+        {
+            Front = Rear = 0;
+            Capacity = size;
+            queue = new int[Capacity];
+        }
+
+        public Queue()  //constructor fara parametru
+        {
+            Front = Rear = 0;
+            Capacity = int.MaxValue;
+            queue = new int[Capacity];
+        }
+
+        // function to insert an element
+        // at the rear of the queue
+        public void Push(int data)
+        {
+            
+            // check queue is full or not
+            if (Capacity == Rear)
+            {
+                Console.WriteLine(" Queue is full ");
+                return;
+            }
+            // insert element at the rear
+            else
+            {
+                queue[Rear] = data;
+                Rear++;
+            }
+            return;
+        }
+        // function to delete an element
+        // from the front of the queue
+        public void Pop()
+        {
+            // if queue is empty
+            if (Front == Rear)
+            {
+                Console.WriteLine(" Queue is empty");
+                return;
+            }
+
+            // shift all the elements from index 2 till rear
+            // to the right by one
+            else
+            {
+                for (int i = 0; i < Rear - 1; i++)
+                {
+                    queue[i] = queue[i + 1];
+                }
+
+                // store 0 at rear indicating there's no element
+                if (Rear < Capacity)
+                    queue[Rear] = 0;
+
+                // decrement rear
+                Rear--;
+            }
+            return;
+        }
+
+        public void Print()
+        {
+            int i;
+            if (Front == Rear)
+            {
+                Console.Write("\nQueue is Empty\n");
+                return;
+            }
+
+            // traverse front to rear and print elements
+            for (i = Front; i < Rear; i++)
+            {
+                Console.Write("{0} <- ", queue[i]);
+            }
+            Console.WriteLine("\n The number of elements in the Queue {0}\n", i);
+            return;
+        }
+
+        public void IsFull()
+        {
+            if (Capacity == Rear)
+            {
+                Console.WriteLine(" Queue is full.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine(" Queue is not full yet.");
+                return;
+            }
+        }
+        public void IsEmpty()
+        {
+            if (Front == Rear)
+            {
+                Console.WriteLine(" Queue is empty.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine(" Queue is not empty.");
+                return;
+            }
+        }
+    }
+}
--- Lab3.Ex3.Program Main.cs
+++ Lab3.Ex3.Program Main.cs
@@ -1,33 +1,49 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace Lab3.Ex_1
+namespace Lab3.ex2
 {
     class Program
     {
-        public static double Surface_Area(Box x)
-        {
-            return 2 * (x.Depth + x.Height + x.Width);
-        }
-        public static double Volume(Box x)
-        {
-            return x.Width * x.Height * x.Depth;
-        }
         static void Main(string[] args)
         {
-            Box Obj1 = new Box();
-            Box Obj2 = new Box(2.5);
-            Box Obj3 = new Box(2.5, 3.75, 4.2);
+            // Create a queue of capacity 5
+            Queue q1 = new Queue(5);
 
-            Console.WriteLine("Box 1\n Width: {0}\n Height: {1}\n Depth: {2}", Obj1.Width, Obj1.Height, Obj1.Depth);
-            Console.WriteLine("Box 2\n Width: {0}\n Height: {1}\n Depth: {2}", Obj2.Width, Obj2.Height, Obj2.Depth);
-            Console.WriteLine("Box 3\n Width: {0}\n Height: {1}\n Depth: {2}", Obj3.Width, Obj3.Height, Obj3.Depth);
-            Console.WriteLine("\n1) Surface Area of the box 1:  " + Surface_Area(Obj1));
-            Console.WriteLine("2) Surface Area of the box 2:  " + Surface_Area(Obj2));
-            Console.WriteLine("3) Surface Area of the box 3:  " + Surface_Area(Obj3));
-            Console.WriteLine("\n1) Volume of the box 1:  " + Volume(Obj1));
-            Console.WriteLine("2) Volume of the box 2:  " + Volume(Obj2));
-            Console.WriteLine("3) Volume of the box 3:  " + Volume(Obj3));
+            //inserting elements in the queue
+            q1.Push(10);
+            q1.Push(20);
+            q1.Push(30);
+            q1.Push(40);
+            q1.Push(50);
+            
+            //print Queue elements
+            Console.Write(" ");
+            q1.Print();
+            q1.IsEmpty();
+            q1.IsFull();
 
+            q1.Pop();
+            q1.Pop();
+            q1.Pop();
+            Console.WriteLine("\n After modifications: ");
+            Console.Write(" ");
+            q1.Print();
+            q1.IsEmpty();
+            q1.IsFull();
+
+            q1.Pop();
+            q1.Pop();
+            Console.WriteLine("\n After modifications: ");
+            Console.Write(" ");
+            q1.IsEmpty();
         }
+
     }
 }
